3ad80272c43289a2c0c4b40806837ddd4f0a16af
danil <piratos163@mail.ru>
2021-11-15 13:06:52 +0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,48 @@
+"use strict";
+
+
+//const result = confirm("Вы здесь?");
+//console.log(result);
+
+
+//const answer = +prompt("Тоби есть 18 лет?", "18");
+//console.log(answer + 5);
+
+
+
+//const answers = [];
+
+//answers[0] = prompt('Какое ваше имя ?', '');
+//answers[1] = prompt('ваша Фамилия ?', '');
+//answers[2] = prompt('Сколько вам лет ?', '');
+
+//console.log(typeof(answers));
+
+
+
+
+//const category = 'toys';
+
+//console.log(`https://someurl.com/${category}/5`);
+
+
+//const user = "Danil";
+//alert(`Привет, ${user}`);
+
+
+
+
+let incr = 10,
+    decr = 10;
+//incr++;
+//decr--;
+
+console.log(++incr);
+console.log(--decr);
+console.log(5%2);
+console.log(2*4 == 8);
+
+const isChecked = true,
+      isClose = false;
+console.log(isChecked && isClose);
+console.log(isChecked || isClose);
